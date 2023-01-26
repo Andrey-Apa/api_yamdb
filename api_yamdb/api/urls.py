2bc7685c9e2b8542167762323fbaad1395f56e64
Maksim Boiko <max.asus1742@gmail.com>
2023-01-26 10:31:31 +0300

    Готов api для жанров

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -2,10 +2,11 @@ from django.urls import include, path
 
 from rest_framework.routers import DefaultRouter
 
-from .views import CategoryViewSet
+from .views import CategoryViewSet, GenreViewSet
 
 v1_router = DefaultRouter()
 v1_router.register('category', CategoryViewSet, basename='category')
+v1_router.register('genre', GenreViewSet, basename='genre')
 
 urlpatterns = [
     path('v1/', include(v1_router.urls)),
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -2,11 +2,17 @@
 
 from .mixins import ListCreateDeleteViewSet
 from .permissions import IsAdminOrReadOnly
-from .serializers import CategorySerializer
-from reviews.models import Category
+from .serializers import CategorySerializer, GenreSerializer
+from reviews.models import Category, Genre
 
 
 class CategoryViewSet(ListCreateDeleteViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+    permission_classes = (IsAdminOrReadOnly,)
+
+
+class GenreViewSet(ListCreateDeleteViewSet):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
     permission_classes = (IsAdminOrReadOnly,)
